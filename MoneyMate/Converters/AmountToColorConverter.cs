a5d921ec319c80a771aa3edb3736a082faad5458
Myo <marie.nicolas29@hotmail.com>
2026-04-05 19:21:09 +0200

    feat: ajout des converters MAUI et enregistrement global dans App.xaml

---
+++ MoneyMate/Converters/AmountToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace MoneyMate.Converters
+{
+    /// <summary>
+    /// Convertit un montant décimal en couleur :
+    /// positif → vert, négatif → rouge.
+    /// </summary>
+    public class AmountToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value is decimal amount)
+                return amount >= 0
+                    ? Color.FromArgb("#6CC57C")
+                    : Color.FromArgb("#E57373");
+
+            return Color.FromArgb("#6CC57C");
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ MoneyMate/Converters/DateToRelativeLabelConverter.cs
@@ -0,0 +1,23 @@
+﻿using MoneyMate.Helpers;
+using System.Globalization;
+
+namespace MoneyMate.Converters
+{
+    /// <summary>
+    /// Convertit une DateTime en libellé relatif lisible.
+    /// Utilise DateHelper.ToRelativeLabel().
+    /// </summary>
+    public class DateToRelativeLabelConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value is DateTime date)
+                return DateHelper.ToRelativeLabel(date);
+
+            return string.Empty;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
+            => throw new NotImplementedException();
+    }
+}

dea3a8070ae769135a89ea311d3ce5bf6971fb79
Myo <marie.nicolas29@hotmail.com>
2026-04-07 23:40:39 +0200

    3.feat: restore session on startup

--- MoneyMate/App.xaml.cs
+++ MoneyMate/App.xaml.cs
@@ -1,15 +1,30 @@
-﻿namespace MoneyMate
+﻿using Microsoft.Extensions.DependencyInjection;
+using MoneyMate.Services.Interfaces;
+
+namespace MoneyMate
 {
     public partial class App : Application
     {
-        public App()
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IAuthenticationService _authenticationService;
+
+        public App(IServiceProvider serviceProvider, IAuthenticationService authenticationService)
         {
+            _serviceProvider = serviceProvider;
+            _authenticationService = authenticationService;
             InitializeComponent();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            _authenticationService.RestoreSession();
+
+            AppShell appShell = _serviceProvider.GetRequiredService<AppShell>();
+            Window window = new(appShell);
+
+            appShell.InitializeForCurrentSession();
+
+            return window;
         }
     }
 }
